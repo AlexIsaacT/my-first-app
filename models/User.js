b128cb275f946b4638c815177918cba235e693e2
Alexisaac7 <alexisaac007@gmail.com>
2019-12-31 19:23:56 +0530

    changed field names

---
+++ models/User.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = (Sequelize, DataTypes) => {
+    var User = Sequelize.define('User', {
+     id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      
+        allowNull: false,
+        primaryKey: true
+      },
+      userName: {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      firstName: {
+           type: DataTypes.STRING,
+           allowNull: true
+      },
+      lastName: {
+            type: DataTypes.STRING,
+            allowNull: true
+      },
+      email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+      },
+      password: {
+            type: DataTypes.STRING,
+            allowNull: true
+      }
+    });
+    return User;
+};
